faff5975edebaf0a97acef781a28e9a0ca716791
Shourya Deep Bera <67041876+User-Unknown005@users.noreply.github.com>
2024-05-18 19:03:33 +0530

    Update MotorControl.ino

--- MotorControl.ino
+++ MotorControl.ino
@@ -1,21 +1,33 @@
-const int irPins[5] = {13,11,12,7,2};
-int irSensors[5] = {0,0,0,0,0}; 
-// B stands for binary value. here it represents 5bit binary.
-void setup(){
-   Serial.begin(9600);
-   for (int i = 0; i < 5; i++)
-   {
-      pinMode(irPins[i], INPUT);
-   }
+const int motorRPin1 = 6;                
+const int motorRPin2 = 9; 
+const int motorLPin1 = 5;            
+const int motorLPin2 = 3;
+
+void setup() {
+  pinMode(motorLPin1,OUTPUT);        
+  pinMode(motorLPin2,OUTPUT);
+  pinMode(motorRPin1,OUTPUT);        
+  pinMode(motorRPin2,OUTPUT);
+
 }
+
 void loop() {
-  for (int count=0;count<5;count++)
-    {
-       irSensors[count]=digitalRead(irPins[count]);
-    } 
-  // Now Displaying the output.
-  for(int i=0;i<5;i++){
-    Serial.print(irSensors[i]);
+  drive(250,250);
+}
+
+void drive(int pwm_A, int pwm_B)
+{
+  set_motor_pwm(pwm_A, motorRPin1, motorRPin2);
+  set_motor_pwm(pwm_B, motorLPin1, motorLPin2);
+}
+void set_motor_pwm(int pwm, int IN1_PIN, int IN2_PIN)
+{
+  if (pwm < 0) {  // reverse speeds
+    analogWrite(IN1_PIN, -pwm);
+    digitalWrite(IN2_PIN, LOW);
+
+  } else { // stop or forward
+    digitalWrite(IN1_PIN, LOW);
+    analogWrite(IN2_PIN, pwm);
   }
-  Serial.println();
 }
